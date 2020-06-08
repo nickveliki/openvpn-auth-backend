77ee4ccd5294c503c036f7af9dcb7a5532786443
NickVeliki <nickveliki@gmail.com>
2020-06-08 21:58:33 +0200

    auth 34

--- app/auth.js
+++ app/auth.js
@@ -1,13 +1,13 @@
 const crypto = require("crypto");
 const fs = require("fs");
-const {key, iv} = JSON.parse(fs.readFileSync("./.secdat").toString());
+const {key, iv} = JSON.parse(fs.readFileSync("../.secdat").toString());
 const {username, password} = process.env;
-const ivf = Buffer.from(JSON.parse(crypto.createDecipheriv("aes-128-gcm", Buffer.from(key, "base64"), Buffer.from(iv, "base64")).update(fs.readFileSync("./udb/definitions.jdf")).toString()).Definitions.filter((item)=>item.split("#")[0].endsWith("user.jdf"))[0].split("#")[1].split(","))
-const user = JSON.parse(crypto.createDecipheriv("aes-128-gcm", Buffer.from(key, "base64"), ivf).update(fs.readFileSync("./udb/user.jdf")).toString()).Versions.filter(({name})=>name===username)[0];
+const ivf = Buffer.from(JSON.parse(crypto.createDecipheriv("aes-128-gcm", Buffer.from(key, "base64"), Buffer.from(iv, "base64")).update(fs.readFileSync("../udb/definitions.jdf")).toString()).Definitions.filter((item)=>item.split("#")[0].endsWith("user.jdf"))[0].split("#")[1].split(","))
+const user = JSON.parse(crypto.createDecipheriv("aes-128-gcm", Buffer.from(key, "base64"), ivf).update(fs.readFileSync("../udb/user.jdf")).toString()).Versions.filter(({name})=>name===username)[0];
 if(!user){
     throw 1
 }else{
-    const {publicKey} = JSON.parse(fs.readFileSync("./.RSA"));
+    const {publicKey} = JSON.parse(fs.readFileSync("../.RSA"));
     const verify = crypto.createVerify("SHA256")
     verify.write(password);
     verify.end();
@@ -17,3 +17,4 @@ if(!user){
         throw 1
     }
 }
+__

2936929855374fc2c4cb5ed29df76f714c96d62f
NickVeliki <nickveliki@gmail.com>
2020-06-08 18:42:51 +0200

    auth 4

--- auth.js
+++ auth.js
@@ -1,13 +1,8 @@
 const jables = require("./app/jableshandler");
 const {username, password} = process.env;
-jables.logEntry(username+":"+password).then(()=>{
-    jables.login({name: username, password}).then((user)=>{
-        jables.logEntry(`${user.name}/${user.uid} logged on to VPN server`).then(()=>{
-            return;
-        })
+jables.login({name: username, password}).then((user)=>{
+        return 0;
     }, (error)=>{
-        jables.logEntry(`${process.env.username} failed login to VPN server`).then(()=>{
-            throw error;
-        })
-    })
+        throw error;
 })
+

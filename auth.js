5b01fc77ce86a96fd82bef9ba060796a364e3206
NickVeliki <nickveliki@gmail.com>
2020-06-08 19:18:05 +0200

    auth 8

--- auth.js
+++ auth.js
@@ -1,12 +1,22 @@
 const jables = require("./app/jableshandler");
 const {username, password} = process.env;
+const action = [];
 try{
     jables.login({name: username, password}).then((user)=>{
-        return 0;
-    }, (error)=>{
-        throw error;
+        action.push("return");
+    }, ()=>{
+        action.push("throw");
 })
 }catch(e){
     throw e;
 }
-
+const interval = setInterval(()=>{
+    if(action.length){
+        clearInterval(interval);
+        if(action[0]==="return"){
+            return
+        }else{
+            throw 1;
+        }
+    }
+}, 200)

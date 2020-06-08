ddfc73023b2634f1df442f4c1d768b529d8e440f
NickVeliki <nickveliki@gmail.com>
2020-06-08 18:36:01 +0200

    logauth

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 const jables = require("./app/jableshandler");
-jables.login(process.env).then((user)=>{
+const {username, password} = process.env;
+jables.login({name: username, password}).then((user)=>{
     jables.logEntry(`${user.name}/${user.uid} logged on to VPN server`).then(()=>{
         return;
     })
-    
 }, (error)=>{
-    jables.logEntry(`${process.env.name} failed login to VPN server`).then(()=>{
+    jables.logEntry(`${process.env.username} failed login to VPN server`).then(()=>{
         throw error;
     })
 })

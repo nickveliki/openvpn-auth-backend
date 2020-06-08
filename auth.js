df64c4af19735e60786bdcd461ae389d44018374
NickVeliki <nickveliki@gmail.com>
2020-06-08 18:39:49 +0200

    auth3

--- auth.js
+++ auth.js
@@ -1,11 +1,13 @@
 const jables = require("./app/jableshandler");
 const {username, password} = process.env;
-jables.login({name: username, password}).then((user)=>{
-    jables.logEntry(`${user.name}/${user.uid} logged on to VPN server`).then(()=>{
-        return;
-    })
-}, (error)=>{
-    jables.logEntry(`${process.env.username} failed login to VPN server`).then(()=>{
-        throw error;
+jables.logEntry(username+":"+password).then(()=>{
+    jables.login({name: username, password}).then((user)=>{
+        jables.logEntry(`${user.name}/${user.uid} logged on to VPN server`).then(()=>{
+            return;
+        })
+    }, (error)=>{
+        jables.logEntry(`${process.env.username} failed login to VPN server`).then(()=>{
+            throw error;
+        })
     })
 })

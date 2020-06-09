9adeeee96cec094b254ed1484609c35055e71632
NickVeliki <nickveliki@gmail.com>
2020-06-09 15:45:12 +0200

    auth 36

--- app/auth.js
+++ app/auth.js
@@ -12,7 +12,7 @@ const logEntry = (logData)=>{
     const p = path.resolve(path.join(__dirname, "../", D.getFullYear().toString()))
     makefolder(p)
     const prefix = (D.getHours()<10?"0":"")+D.getHours()+":"+(D.getMinutes()<10?"0":"")+D.getMinutes()+":"+(D.getSeconds()<10?"0":"")+D.getSeconds()
-    fs.writeFileSync(p+"/"+(D.getMonth()<9?"0":"")+(D.getMonth()+1)+"_"+(D.getDate()<10?"0":"")+D.getDate()+".vpnlog", prefix+": "+logData+"\r\n", {flag:"a"});
+    fs.writeFileSync(p+"/"+(D.getMonth()<9?"0":"")+(D.getMonth()+1)+"_"+(D.getDate()<10?"0":"")+D.getDate()+".log", prefix+": "+logData+"\r\n", {flag:"a"});
 }
 const {key, iv} = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../.secdat")).toString());
 const {username, password} = process.env;
--- app/disconnect.js
+++ app/disconnect.js
@@ -11,7 +11,6 @@ const logEntry = (logData)=>{
     const p = path.resolve(path.join(__dirname, "../", D.getFullYear().toString()))
     makefolder(p)
     const prefix = (D.getHours()<10?"0":"")+D.getHours()+":"+(D.getMinutes()<10?"0":"")+D.getMinutes()+":"+(D.getSeconds()<10?"0":"")+D.getSeconds()
-    fs.writeFileSync(p+"/"+(D.getMonth()<9?"0":"")+(D.getMonth()+1)+"_"+(D.getDate()<10?"0":"")+D.getDate()+".vpnlog", prefix+": "+logData+"\r\n", {flag:"a"});
+    fs.writeFileSync(p+"/"+(D.getMonth()<9?"0":"")+(D.getMonth()+1)+"_"+(D.getDate()<10?"0":"")+D.getDate()+".log", prefix+": "+logData+"\r\n", {flag:"a"});
 }
-console.log(process.env)
-logEntry(`${process.env.commonname} has disconnected`)
+logEntry(`${process.env.common_name} has disconnected`)
